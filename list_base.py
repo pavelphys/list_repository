398590a888a68f3710a43306beb1fb2f619977e3
AlbertMatseiko <111632813+AlbertMatseiko@users.noreply.github.com>
2022-09-23 19:50:27 +0300

    Update list_base.py

--- list_base.py
+++ list_base.py
@@ -7,3 +7,45 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+
+class Bird:
+    
+    objInstancesCount = 0
+    
+    def __init__(self, name, id, age):
+        self._name = name
+        self._id = id
+        self._age = age
+        Bird.objInstancesCount = Bird.objInstancesCount + 1 
+        
+    # getter
+    @property
+    def name(self):
+            return self._name
+    # setter
+    @name.setter
+    def name(self, n):
+            if(n != "Иван"):
+                self._name = n
+    
+    # getter
+    @property
+    def age(self):
+            return self._age
+    # setter
+    @age.setter
+    def name(self, age):
+            if(age >= 0):
+                self._age = age
+            else:
+                self._age = 0
+    
+    # getter
+    @property
+    def id(self):
+            return self._id
+        
+    def info(self):
+        print("Имя: " + self._name)
+        print("Идентификационный номер: " + str(self._id))
+        print("Возраст: " + str(self._age))
